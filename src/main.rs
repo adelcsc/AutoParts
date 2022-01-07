8c05ab7c5f477730fbf66e7c96f575244fcbd665
Adel Benhamida <a.benhamida.del@gmail.com>
2022-01-07 09:25:49 +0100

    feat(graphql): Added playground and graphql endpoints refactor(graphql): added AlterNames Object

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,43 @@ mod config;
 mod controllers;
 mod entities;
 
-use actix_web::{App, HttpServer};
-use sea_orm::Database;
+use std::io::Bytes;
+use actix_web::{App, guard, HttpResponse, HttpServer, web};
+use actix_web::web::Data;
+use async_graphql::{EmptyMutation, EmptySubscription, Schema, SchemaBuilder};
+use async_graphql::dataloader::DataLoader;
+use async_graphql::http::{GraphQLPlaygroundConfig, playground_source};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use sea_orm::{Database, DatabaseConnection, EntityTrait};
 use crate::config::config;
+use crate::entities::{Query, SqliteLoader};
+
+struct AppState {
+    schema : Schema<Query,EmptyMutation,EmptySubscription>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let conn=Database::connect("sqlite:app.db").await.unwrap();
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
             .configure(config)
+            .service(web::resource("/").guard(guard::Post()).to(graph))
+            .service(web::resource("/").guard(guard::Get()).to(playground))
+            .app_data(Data::new(AppState{
+                schema : Schema::build(Query,EmptyMutation,EmptySubscription)
+                    .data(DataLoader::new(SqliteLoader{pool: conn.to_owned()},actix_rt::spawn)).finish(),
+            }))
     })
     .bind("192.168.1.100:8080").unwrap()
     .run()
     .await
+}
+
+async fn graph(req : GraphQLRequest,data : web::Data<AppState>) ->GraphQLResponse {
+    let res = data.schema.execute(req.into_inner()).await;
+    res.into()
+}
+async fn playground() ->HttpResponse {
+    HttpResponse::Ok().content_type("text/html; charset=utf-8").body(playground_source(GraphQLPlaygroundConfig::new("/").subscription_endpoint("/")))
 }
